0a5b1893788cbf757c5e0e8a768537363677b2ba
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-27 23:42:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,6 @@ def assign_rooms(names)
 end
 
 def printer(attendees)
-  batch_badge_creator(names).each {|name| puts name}
+  batch_badge_creator(name).each {|name| puts name}
   assign_rooms(names).each {|rooms| puts index}
 end
