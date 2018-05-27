0aedbb4d8347c39384a65ac49912bd87ae3c3c3b
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-27 23:50:54 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,8 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  names.collect do |name|
+def batch_badge_creator(attendees)
+  attendees.collect do |name|
     "Hello, my name is #{name}."
   end
 end
